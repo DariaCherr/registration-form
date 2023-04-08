508f72b39bdc19ffda46ed08142158ca6816ca6a
DariaCherr <DaskhenCher@mail.ru>
2023-04-08 23:29:40 +0300

    version2

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-const button = document.querySelector(".btn");
-document.querySelector(".text").hidden = false;
+let el_1 = document.querySelectorAll('.firstName');
+let el_2 = document.querySelectorAll('.lastName');
+let el_3 = document.querySelectorAll('.nationality');
+let el_4 = document.querySelectorAll('.userEmail');
+let el_5 = document.querySelectorAll('.date');
+let el_6 = document.querySelectorAll('.gender');
+let el_7 = document.querySelectorAll('.userPassword');
+let el_8 = document.querySelectorAll('.confirmPassword');
 
-button.addEventListener('click', () => {
-    document.querySelector("#finish").classList.remove("hidden");
-    document.querySelector(".right-side").hidden = true;
-    document.querySelector(".left-side").hidden = true;
-    document.querySelector("h1").hidden = true;
-    document.querySelector(".description").hidden = true;
+function visibleInput(el) {
+    el[0].classList.add('form__card_visible');
+}
 
-})
+setInterval(() => { visibleInput(el_1) }, 50);
+setInterval(() => { visibleInput(el_2) }, 50);
+setInterval(() => { visibleInput(el_3) }, 700);
+setInterval(() => { visibleInput(el_4) }, 700);
+setInterval(() => { visibleInput(el_5) }, 1400);
+setInterval(() => { visibleInput(el_6) }, 1400);
+setInterval(() => { visibleInput(el_7) }, 2000);
+setInterval(() => { visibleInput(el_8) }, 2000);
+
+clearInterval(setInterval(() => { visibleInput(el_8) }, 2000));
